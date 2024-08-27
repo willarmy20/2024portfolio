2b790eefdce4f3b149bc1a6d29eb5e4853e725dc
Que Williams <quinta750@gmail.com>
2024-08-27 16:34:17 -0400

    updating

---
+++ src/Experience.jsx
@@ -0,0 +1,88 @@
+import {Text,Html,ContactShadows,PresentationControls,Float,Environment,useGLTF,OrbitControls,} from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import { useRef, useState } from "react";
+import * as THREE from 'three';
+
+
+export default function Experience() {
+  const computer = useGLTF(
+    "https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/macbook/model.gltf"
+  );
+  console.log(computer);
+
+  // const computerRef = useRef();
+  // const [hovered, setHovered] = useState(false); 
+
+  // useFrame((state,delta) => {
+  //   if(computerRef.current) {
+  //       computerRef.current.scale.x = THREE.MthUtils.lerp(computerRef.current.scale.x, hovered ? 1.4 : 1, 0.1)
+  //       computerRef.current.scale.y = THREE.MthUtils.lerp(computerRef.current.scale.y, hovered ? 1.4 : 1, 0.1)
+  //       computerRef.current.scale.z = THREE.MthUtils.lerp(computerRef.current.scale.z, hovered ? 1.4 : 1, 0.1)
+  //       computerRef.current.position.z = THREE.MthUtils.lerp(computerRef.current.position.z, hovered ? -1.5 : 0, 0.1)
+  //   }
+  // });
+  return (
+    <>
+      <Environment preset="city" />
+
+      <color args={["#241a1a"]} attach="background" />
+
+      <OrbitControls makeDefault />
+
+      <PresentationControls 
+      global
+      rotation={ [ 0.13, 0.1, 0 ] }
+      polar={ [ - 0.4, 0.2 ] }
+      azimuth={ [ - 1, 0.75 ] }
+      config={ { mass: 2, tension: 400 } }
+      snap={ { mass: 4, tension: 400 } }
+      >
+        <Float rotationIntensity={0.4}>
+        <rectAreaLight
+        width={ 2.5 }
+        height={ 1.65 }
+        intensity={ 65 }
+        color={ '#ff6900' }
+        rotation={ [ - 0.1, Math.PI, 0 ] }
+        position={ [ 0, 0.55, - 1.15 ] }
+    />
+
+        <primitive
+            // ref={computerRef}
+            object={ computer.scene }
+            position-y={ - 1.2 }
+            rotation-x={ 0.13 }
+            // onPointerOver={()=> setHovered(true)}
+            // onPointerOut={()=> setHovered(false)}
+        >
+            <Html 
+            transform
+            wrapperClass="htmlScreen"
+            distanceFactor={ 1.17 }
+            position={ [ 0, 1.56, - 1.4 ] }
+            rotation-x={ - 0.256 }
+            // scale={hovered ? 1.4 : 1}
+            >
+            <iframe src="https://htmlsite.onrender.com/" />
+            </Html>
+        </primitive>
+        <Text
+        font="./bangers-v20-latin-regular.woff"
+        fontSize={ 1 }
+        position={ [ 2, 0.75, 0.75 ] }
+        rotation-y={ - 1.25 }
+        maxWidth={ 2 }
+        >
+            Laquinta Williams
+        </Text>
+        </Float>
+      </PresentationControls>
+      <ContactShadows
+       position-y={ - 1.4 }
+       opacity={ 0.4 }
+       scale={ 5 }
+       blur={ 2.4 }
+        />
+    </>
+  );
+}
